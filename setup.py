09d1852edd0579ab3d53348ed347ea378894327a
Ambigapath <168761904+Ambigapath@users.noreply.github.com>
2024-09-17 14:10:54 +0530

    added logging and exception

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
+from typing import List
 
+# Define the string '-e .' which may be present in requirements.txt
+HYPHEN_E_DOT = '-e .'
 
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        
+        # Remove '-e .' if it's in the requirements
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
 
 setup(
     name='mlproject',
@@ -8,9 +22,5 @@ setup(
     author='Ambigapathi',
     author_email='ambigapathikavin@gmail.com',
     packages=find_packages(),
-    install_requires=[
-        'numpy',
-        'pandas',
-        'scikit-learn',
-    ],
+    install_requires=get_requirements('requirements.txt')
 )
